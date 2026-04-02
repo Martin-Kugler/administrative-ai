0c12bb88f00b76f997a5df04810de310ca1bb262
Martin-Kugler <martinkugler97@gmail.com>
2026-04-02 18:10:55 +0200

    A new test document inserted.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ Settings.llm = OpenAI(
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 # Read the PDF in the respective folder: 
-document_path = "./documents"
+document_path = "documents"
 print("Loading documents...")
 documents = SimpleDirectoryReader(document_path).load_data()
 
